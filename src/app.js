c351f9919c13f9c3207de19f82e6e456c5076132
Andrés Miranda <31228376+andresmiranda2000@users.noreply.github.com>
2023-12-12 05:55:54 +0000

    card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,63 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generarCarta() {
+  const palos = ["spade", "club", "heart", "diamond"];
+  const valores = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const carta = document.getElementById("carta");
+  const paloAleatorio = palos[Math.floor(Math.random() * 4)];
+  const valorAleatorio = valores[Math.floor(Math.random() * 13)];
+
+  carta.className = "card";
+  carta.classList.add(paloAleatorio);
+
+  carta.innerHTML = `
+    <div class="numero">${valorAleatorio}</div>
+    <div class="palo-top-left">${getPaloSymbol(paloAleatorio)}</div>
+    <div class="palo-bottom-right">${getPaloSymbol(paloAleatorio)}</div>
+  `;
+
+  if (paloAleatorio === "heart" || paloAleatorio === "diamond") {
+    carta.style.color = "red";
+  } else {
+    carta.style.color = "black";
+  }
+}
+
+function getPaloSymbol(palo) {
+  switch (palo) {
+    case "spade":
+      return "♠";
+    case "club":
+      return "♣";
+    case "heart":
+      return "♥";
+    case "diamond":
+      return "♦";
+    default:
+      return "";
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  generarCarta();
 };
+
+document.getElementById("carta").addEventListener("click", function() {
+  generarCarta();
+});
